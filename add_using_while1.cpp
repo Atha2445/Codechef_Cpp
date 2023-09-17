a4aaab76214987c39b0d56bb8b93de42077f304b
ATHARVA YEOLA <114800045+Atha2445@users.noreply.github.com>
2023-09-17 15:22:30 +0530

    Create add_using_while1.cpp

---
+++ add_using_while1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t != 0){
+	    int a,b;
+	    cin>>a>>b;
+	    cout<<a+b<<endl;
+	    t--;
+	}
+	return 0;
+}
