fe93df99c1d98b82fbba409b85daad51e86ccedb
ATHARVA YEOLA <114800045+Atha2445@users.noreply.github.com>
2023-09-17 18:07:15 +0530

    Create roller_coaster.cpp

---
+++ roller_coaster.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t>0){
+	    int x,h;
+	    cin>>x>>h;
+	    if(x>=h){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	    
+	    
+	        
+	    
+	   
+	    t--;
+	}
+	return 0;
+}
