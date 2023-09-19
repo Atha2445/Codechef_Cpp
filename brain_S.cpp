6a2948c40a0bb7efdf077ce214251d157449ee7e
ATHARVA YEOLA <114800045+Atha2445@users.noreply.github.com>
2023-09-19 09:09:15 +0530

    Create brain_S.cpp

---
+++ brain_S.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X,Y;
+	cin>>X>>Y;
+	if(X==Y){
+	    cout<<"NO"<<endl;
+	    
+	}
+	else if(X<Y){
+	    cout<<"YES"<<endl;
+	}
+	else if(X>Y){
+	    
+	    cout<<"NO"<<endl;
+	}
+	    
+	
+	
+	return 0;
+}
